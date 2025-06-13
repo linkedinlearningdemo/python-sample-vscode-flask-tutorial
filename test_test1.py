40a40a1c13cd9387161ed7ea2e2e14eaeaf7cf54
linkedinlearningdemo <odl_user_1709078@azurehol1003.onmicrosoft.com>
2025-06-13 11:26:52 +0900

    Update test_test1.py

--- test_test1.py
+++ test_test1.py
@@ -2,3 +2,31 @@ import hashlib
 password = "mypassword!"
 hashed = hashlib.md5(password.encode()).hexdigest()
 print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
+import hashlib
+password = "mypassword!"
+hashed = hashlib.md5(password.encode()).hexdigest()
+print(hashed)
